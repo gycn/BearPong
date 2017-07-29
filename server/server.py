a1a6ac511db2f2b8e47769651989240fc0e3469c
Gary <gary@mumec.com>
2017-07-29 15:01:47 -0700

    wrote initial version of server that can receive packets

--- server/server.py
+++ server/server.py
@@ -2,50 +2,83 @@ from twisted.internet.protocol import Protocol
 from twisted.internet.protocol import Factory
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.internet import reactor
+from twisted.internet.task import LoopingCall
+import struct
 
-class Echo(Protocol):
+UPDATE_USER_SPATIAL_INFORMATION_OPCODE = 0x00
+SELECT_OBJECT_OPCODE = 0x01
 
-    def __init__(self, factory):
+UPDATE_OBJECT_SPATIAL_INFORMATION_OPCODE = 0x10
+SEND_USER_ID_OPCODE = 0x11
+SELECT_OBJECT_RESPONSE_OPCODE = 0x12
+
+def get_opcode(bytes_in):
+    return bytes_in[0]
+
+def convert_bytes_to_int(byte_string):
+    return int.from_bytes(b, byteorder='big', signed=False)
+
+def convert_bytes_to_float(byte_string):
+    return struct.unpack('f', byte_string)[0]
+
+class ARWebServerProtocol(Protocol):
+
+    def __init__(self, factory, addr, scene):
         self.factory = factory
-        self.opcodes = {
-            '0x00': self.update_user
-            '0x01': self.select_object
-            '0x02': self.man_object
-            '0x10': self.update_object
+        self.addr = addr
+        self.scene = scene
+        self.user = None
+        self.OPCODE_FUNCTIONS = {
+          UPDATE_USER_SPATIAL_INFORMATION_OPCODE: self.handle_new_user_spatial_information,
+          SELECT_OBJECT_OPCODE: self.handle_object_selection
         }
-        self.objects
-
-    def read_opcode(self, data):
-        return data[0]
-
-    def update_user(self):
-
-    def man_object(self):
-
-    def update_object(self):
-
-
 
     def connectionMade(self):
         self.factory.numProtocols = self.factory.numProtocols + 1
-        self.transport.write("New connection".encode())
+        print('New Connection')
+        
+        ##Add new user to scene
+        #self.user = self.scene.new_user(self)
+        ##Send user id back
+        #self.transport.send(bytearray([SEND_USER_ID_OPCODE, self.user.id]))
 
     def connectionLost(self, reason):
         self.factory.numProtocols = self.factory.numProtocols - 1
+        #self.scene.remove_user(user)
 
     def dataReceived(self, data):
-        self.transport.write(data)
-        print(data)
+        opcode = get_opcode(data)
+        if (opcode in self.OPCODE_FUNCTIONS):
+          self.OPCODE_FUNCTIONS[opcode](data)
+        else:
+          print('invalid command')
 
+    def handle_new_user_spatial_information(self, data):
+        new_position = [convert_bytes_to_float(data[i:i + 4]) for i in range(1, 13, 4)] 
+        new_direction = [convert_bytes_to_float(data[i:i + 4]) for i in range(13, 25, 4)] 
+        print(new_position, new_direction)
+        #self.user.on_new_user_spatial_information(new_position, new_direction)
 
-class EchoFactory(Factory):
-    def __init__(self):
+    def handle_object_selection(self, data):
+        object_id = convert_bytes_to_int(data[1:5])
+        print(data[1:5])
+        print(object_id)
+    #    success = self.scene.get_object(object_id).on_object_select(user)
+    #    self.transport.send(bytearray([SELECT_OBJECT_RESPONSE_OPCODE, success]))
+
+class ARWebServerFactory(Factory):
+
+    def __init__(self, scene):
         self.numProtocols = 0
+        
+        #Scene contains the user and object information
+        self.scene = scene 
 
     def buildProtocol(self, addr):
-        return Echo(self)
+        return ARWebServerProtocol(self, addr, self.scene)
 
-# 8007 is the port you want to run under. Choose something >1024
-endpoint = TCP4ServerEndpoint(reactor, 8007)
-endpoint.listen(EchoFactory())
-reactor.run()
+if __name__ == '__main__':
+    # 8007 is the port you want to run under. Choose something >1024
+    endpoint = TCP4ServerEndpoint(reactor, 8007)
+    endpoint.listen(ARWebServerFactory(None))
+    reactor.run()
