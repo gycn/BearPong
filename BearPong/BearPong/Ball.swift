491a62b050a16bff5962cee6bbecd33666ccb450
Walt <walt.leungwbl@berkeley.edu>
2017-07-29 20:29:56 -0700

    client <==> server

--- BearPong/BearPong/Ball.swift
+++ BearPong/BearPong/Ball.swift
@@ -10,6 +10,7 @@ import UIKit
 import SceneKit
 
 class Ball: ARObject {
+    var velocity:SCNVector3
     override init () {
         super.init()
         let sphere = SCNSphere(radius: 0.05)
@@ -17,10 +18,11 @@ class Ball: ARObject {
         let shape = SCNPhysicsShape(geometry: sphere, options: nil)
         self.physicsBody = SCNPhysicsBody(type: .dynamic, shape: shape)
         self.physicsBody?.isAffectedByGravity = false
-        
+
         self.physicsBody?.categoryBitMask = ViewController.CollisionCategory.ball.rawValue
         self.physicsBody?.contactTestBitMask = ViewController.CollisionCategory.user.rawValue
     }
+    
     
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
--- BearPong/BearPong/ViewController.swift
+++ BearPong/BearPong/ViewController.swift
@@ -16,8 +16,8 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
 
     @IBOutlet var textLabel: UILabel!
     @IBOutlet var sceneView: ARSCNView!
-    var addr = "192.168.0.103"
-    var prt = 8007
+    
+    
     var client:TCPClient = TCPClient(address: "192.168.0.103", port: Int32(8007))
     var doesBallExist = false
     
@@ -32,9 +32,22 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
     var receivedOpcode: Int = 0
     var sentInstruction: [UInt8] = []
     
+    // New Ball Object
+    var ballNode = Ball()
+    
+    // UserID
+    var userID: Int = 0
+    
+    // Frames per second or refresh rate
+    var FPS: Int = 15
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        // Hide yo balls
+        ballNode.isHidden = true
+
         
         // Setup gesture
         let tap = UITapGestureRecognizer(target: self, action: #selector(self.handleTap(_:)))
@@ -62,18 +75,16 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
     func socketSetup() {
         switch client.connect(timeout: 3) {
         case .success:
-            print("success!")
-            sendData()
-            Timer.scheduledTimer(timeInterval: 0.05, target: self, selector: #selector(ViewController.sendData), userInfo: nil, repeats: true)
-            Timer.scheduledTimer(timeInterval: 0.10, target: self, selector: #selector(ViewController.receiveData), userInfo: nil, repeats: true)
+            // Start sending and receiving data at FPS intervals
+            Timer.scheduledTimer(timeInterval: 1/FPS, target: self, selector: #selector(ViewController.sendData), userInfo: nil, repeats: true)
+            Timer.scheduledTimer(timeInterval: 1/FPS, target: self, selector: #selector(ViewController.receiveData), userInfo: nil, repeats: true)
         case .failure(let error):
-            print("error2")
             print(error)
         }
     }
     
     @objc func sendData() {
-        switch client.send(string: "fuck you") {
+        switch client.send(data: sentInstruction) {
         case .success:
             print("success2")
         case .failure(let error):
@@ -101,12 +112,30 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
             return
         case UPDATE_OBJECT_SPATIAL_INFORMATION_OPCODE:
             let objectID = byteArrayToInt(byteArray: Array(instruction[2...9]))
-            let position = byteArrayToFloat(byteArray: Array(instruction[10...33]))
-            let orientation = byteArrayToFloat(byteArray: Array(instruction[34...57]))
-            let velocity = byteArrayToFloat(byteArray: Array(instruction[58...81]))
-
+            
+            let positionX = byteArrayToFloat(byteArray: Array(instruction[10...17]))
+            let positionY = byteArrayToFloat(byteArray: Array(instruction[18...25]))
+            let positionZ = byteArrayToFloat(byteArray: Array(instruction[26...33]))
+            let position = SCNVector3(positionX, positionY, positionZ)
+            
+            let orientationX = byteArrayToFloat(byteArray: Array(instruction[34...41]))
+            let orientationY = byteArrayToFloat(byteArray: Array(instruction[42...49]))
+            let orientationZ = byteArrayToFloat(byteArray: Array(instruction[50...57]))
+            let orientation = SCNVector3(orientationX, orientationY, orientationZ)
+            
+            let velocityX = byteArrayToFloat(byteArray: Array(instruction[58...65]))
+            let velocityY = byteArrayToFloat(byteArray: Array(instruction[66...73]))
+            let velocityZ = byteArrayToFloat(byteArray: Array(instruction[74...81]))
+            let velocity = SCNVector3(velocityX, velocityY, velocityZ)
+            
+            ballNode.position = position
+//            ballNode.orientation = orientation
+            ballNode.velocity = velocity
+            return
+            
         case SEND_USER_ID_OPCODE:
             let userID = byteArrayToInt(byteArray: Array(instruction[2...9]))
+            self.userID = userID
 
         case SELECT_OBJECT_RESPONSE_OPCODE:
             let response = byteArrayToInt(byteArray: Array(instruction[2...3]))
@@ -116,21 +145,30 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
         }
     }
     
+    func vectorToByteArray(vector: SCNVector3) -> [UInt8] {
+        let X = floatToByteArray(value: vector.x)
+        let Y = floatToByteArray(value: vector.y)
+        let Z = floatToByteArray(value: vector.z)
+        return X + Y + Z
+    }
+
     // Update User Spatial Information
-    func updateUserSpatialInformaiton(position: Int, orientation: Float) -> [UInt8] {
+    func updateUserSpatialInformation(position: Float, orientation: Float) {
         let opcodeArray = intToByteArray(value: 00)
-        let positionArray = intToByteArray(value: position)
+        let positionArray = floatToByteArray(value: position)
+        
+        // orientation needs to be 4D
         let orientationArray = floatToByteArray(value: orientation)
         let instruction = opcodeArray + positionArray + orientationArray
-        return instruction
+        sentInstruction = instruction
     }
     
     // Select Object
-    func selectObject(objectID: Int) -> [UInt8] {
+    func selectObject(objectID: Int) {
         let opcodeArray = intToByteArray(value: 00)
         let objectIDArray = intToByteArray(value: objectID)
         let instruction = opcodeArray + objectIDArray
-        return instruction
+        sentInstruction = instruction
     }
     
     override func viewWillAppear(_ animated: Bool) {
@@ -191,7 +229,7 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
         if doesBallExist == true {
             return
         }
-        let ballNode = Ball()
+        ballNode.isHidden = false
         let (direction, position) = self.getUserVector()
         ballNode.position = position
         
@@ -231,12 +269,12 @@ class ViewController: UIViewController, ARSCNViewDelegate, SCNPhysicsContactDele
     func getUserVector() -> (SCNVector3, SCNVector3) {
         if let frame = self.sceneView.session.currentFrame {
             let mat = SCNMatrix4(frame.camera.transform)
-            let dir = SCNVector3(-1 * mat.m31, -1 * mat.m32, -1 * mat.m33)
+            let ori = SCNVector3(-1 * mat.m31, -1 * mat.m32, -1 * mat.m33)
             let pos = SCNVector3(mat.m41, mat.m42, mat.m43)
-            
-            return (dir, pos)
+            updateUserSpatialInformation(position: vectorToByteArray(vector: pos), orientation: vectorToByteArray(vector: ori))
+            return (pos, ori)
         }
-        return (SCNVector3(0, 0, -1), SCNVector3(0, 0, -0.2))
+        return (SCNVector3(0, 0, -0.2), SCNVector3(0, 0, -1))
     }
     
     func byteArrayToFloat(byteArray: [UInt8]) -> Float {
