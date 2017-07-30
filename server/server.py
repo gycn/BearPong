22120b265b24fb71b62acd65e2f166f7ab9be136
Gary <gary@mumec.com>
2017-07-29 20:46:42 -0700

    fixed looping

--- server/server.py
+++ server/server.py
@@ -4,22 +4,17 @@ from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.internet import reactor
 from twisted.internet.task import LoopingCall
 
+import numpy as np
 import struct
-from scene import Scene
 
-UPDATE_USER_SPATIAL_INFORMATION_OPCODE = 0x00
-SELECT_OBJECT_OPCODE = 0x01
+import packet_codes
 
-UPDATE_OBJECT_SPATIAL_INFORMATION_OPCODE = 0x10
-SEND_USER_ID_OPCODE = 0x11
-SELECT_OBJECT_RESPONSE_OPCODE = 0x12
 
-SEND_IMPLEMENTATION_SPECIFIC_MESSAGE = 0xFF 
 def get_opcode(bytes_in):
     return bytes_in[0]
 
 def convert_bytes_to_int(byte_string):
-    return int.from_bytes(b, byteorder='big', signed=False)
+    return int.from_bytes(byte_string, byteorder='big', signed=False)
 
 def convert_bytes_to_float(byte_string):
     return struct.unpack('f', byte_string)[0]
@@ -32,9 +27,9 @@ class ARWebServerProtocol(Protocol):
         self.scene = scene
         self.user = None
         self.OPCODE_FUNCTIONS = {
-          UPDATE_USER_SPATIAL_INFORMATION_OPCODE: self.handle_new_user_spatial_information,
-          SELECT_OBJECT_OPCODE: self.handle_object_selection,
-          SEND_IMPLEMENTATION_SPECIFIC_MESSAGE: self.handle_implementation_specific_message
+          packet_codes.UPDATE_USER_SPATIAL_INFORMATION_OPCODE: self.handle_new_user_spatial_information,
+          #packet_codes.SELECT_OBJECT_OPCODE: self.handle_object_selection,
+          packet_codes.SEND_IMPLEMENTATION_SPECIFIC_MESSAGE: self.handle_implementation_specific_message
         }
 
     def connectionMade(self):
@@ -44,28 +39,37 @@ class ARWebServerProtocol(Protocol):
         #Add new user to scene
         self.user = self.scene.new_user(self)
         #User creation was not successful
-        if user.id == 0xFFFFFFFF:
+        if self.user.id == 0xFFFFFFFF:
             self.loseConnection()
         #User creation successful
         else:
           #Send user id back
-          self.transport.write(bytearray([SEND_USER_ID_OPCODE, self.user.id]))
+          self.transport.write(bytearray([packet_codes.SEND_USER_ID_OPCODE, self.user.id]))
 
     def connectionLost(self, reason):
-        self.factory.numProtocols = self.factory.numProtocols - 1
+        #remove user from scene upon disconnect
         self.scene.remove_user(self.user)
 
     def dataReceived(self, data):
-        opcode = get_opcode(data)
-        if (opcode in self.OPCODE_FUNCTIONS):
-          self.OPCODE_FUNCTIONS[opcode](data)
-        else:
-          print('invalid command')
+        while (len(data) > 0):
+          opcode = get_opcode(data)
+          if (opcode in self.OPCODE_FUNCTIONS):
+            #Get next packet length
+            if opcode in packet_codes.COMMAND_LENGTHS:
+              packet_length = packet_codes.COMMAND_LENGTHS[opcode]
+              self.OPCODE_FUNCTIONS[opcode](data[:packet_length + 1])
+              data = data[packet_length + 1:]
+            else:
+              packet_length = data[1] 
+              self.OPCODE_FUNCTIONS[opcode](data[2:packet_length + 2])
+              data = data[packet_length + 2:]
+          else:
+            print('invalid command')
 
     def handle_new_user_spatial_information(self, data):
         new_position = np.array([convert_bytes_to_float(data[i:i + 4]) for i in range(1, 13, 4)]) 
         new_direction = np.array([convert_bytes_to_float(data[i:i + 4]) for i in range(13, 25, 4)]) 
-        print(new_position, new_direction)
+        print('New User Spatial Information: {} {}'.format(new_position, new_direction))
         self.user.on_new_user_spatial_information(new_position, new_direction)
 
     def handle_object_selection(self, data):
@@ -73,10 +77,10 @@ class ARWebServerProtocol(Protocol):
         print(data[1:5])
         print(object_id)
         success = self.scene.get_object(object_id).on_object_select(user)
-        self.transport.write(bytearray([SELECT_OBJECT_RESPONSE_OPCODE, success]))
+        self.transport.write(bytearray([packet_codes.SELECT_OBJECT_RESPONSE_OPCODE, success]))
     
     def handle_implementation_specific_message(self, data):
-        self.scene.on_implementation_specific_message(data[1:])
+        self.user.on_implementation_specific_message(data)
 
 class ARWebServerFactory(Factory):
 
@@ -93,11 +97,16 @@ class ARServer:
     def __init__(self, scene, port, main_loop_interval = 0.017):
         self.endpoint = TCP4ServerEndpoint(reactor, port)
         self.endpoint.listen(ARWebServerFactory(scene))
-        self.main_loop = LoopingCall(scene.main_loop)
+        self.scene = scene
         self.main_loop_interval = main_loop_interval
+    
+    def main_loop(self):
+        #self.scene.main_loop()
+        self.scene.send_object_updates()
 
     def start(self):
-        #self.main_loop.start(self.main_loop_interval)
+        loop = LoopingCall(self.main_loop)
+        loop.start(self.main_loop_interval)
         reactor.run()
 
 
