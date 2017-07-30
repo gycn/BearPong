6283860a452912558b5b43f4a2b8c732ae1809c1
Gary <gary@mumec.com>
2017-07-29 17:39:38 -0700

    finished initial implementation of bearpong

--- server/scene.py
+++ server/scene.py
@@ -3,8 +3,8 @@ import numpy as np
 
 class Scene:
     def __init__(self):
-        self.objects = {}
-        self.users = {}
+        self.objects = [] 
+        self.users = []
         self.user_count = 0
         self.object_count = 0
 
@@ -35,7 +35,7 @@ class Scene:
         else:
             self.generate_user_id(user)
             print('ADDING USER ID: {}'.format(user.id))
-            self.users[user.id] = user
+            self.users.append(user)
 
     def get_user(self, idx):
         return self.users[idx]
@@ -53,7 +53,10 @@ class Scene:
         del self.objects[obj.id]
     
     def main_loop(self):
-      raise NotImplemented('Implement this')
+        raise NotImplemented('Implement this')
+
+    def on_implementation_specific_message(self, msg):
+        raise NotImplemented
     # def new_object(self, protocol):
     #     # instantiate with zeros for direction/position
     #     new = ar_object.AR_Object(np.zeros((1,3)), np.zeros((1,3)), protocol)
@@ -61,9 +64,13 @@ class Scene:
     #     assert new.id, 'New User must have an ID'
     #     return new
     #
-    # def add_object(self, obj):
-    #     if obj in self.obj:
-    #         raise Exception('Object already exists!')
-    #     else:
-    #         self.generate_user_id(user)
-    #         self.users[user.id] = user
+    def add_object(self, obj):
+        if obj in self.obj:
+            raise Exception('Object already exists!')
+        else:
+            self.generate_user_id(user)
+            self.objects.append(obj)
+            
+    def send_message_to_all_users(self, msg):
+         for user in self.users:
+             self.user.protocol.write(msg)
